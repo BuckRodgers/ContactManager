b7993414fce3ea45d1a5347ddd3f4d0f0c0e3ea1
Ryan <lee.ryanjohn@gmail.com>
2014-06-12 13:08:05 +0200

    moved /Contact from /index
    To add contacts domain/Contact/create
    To see contact domain/Contact/
    
    This is not the database, but a contact repository service.

---
+++ MvcRestfulContactManager/Controllers/ContactApiController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net;
+using System.Web.Http;
+using MvcRestfulContactManager.Services;
+using System.Net.Http;
+using MvcRestfulContactManager.Models;
+
+namespace MvcRestfulContactManager.Controllers
+{
+    public class ContactApiController : ApiController
+    {
+         private ContactRepository contactRepository;
+
+        public ContactApiController()
+        {
+            this.contactRepository = new ContactRepository();
+        }
+
+        public HttpResponseMessage Post(Contact contact)
+        {
+            this.contactRepository.SaveContact(contact);
+
+            var response = Request.CreateResponse<Contact>(System.Net.HttpStatusCode.Created, contact);
+
+            return response;
+        }
+
+    }
+}
--- MvcRestfulContactManager/Controllers/ContactController.cs
+++ MvcRestfulContactManager/Controllers/ContactController.cs
@@ -3,27 +3,67 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
-using System.Net.Http;
-using System.Web.Http;
+using System.Net;
+using System.Web;
 using MvcRestfulContactManager.Services;
+using System.Web.Mvc;
+using System.Data.Entity;
 
 namespace MvcRestfulContactManager.Controllers
-{
-    public class ContactController : ApiController
+{// : ApiController
+    public class ContactController : Controller //ApiController //Controller for views, ApiController for JSON Post and get
     {
 
         private ContactRepository contactRepository;
 
         public ContactController()
         {
+            
             this.contactRepository = new ContactRepository();
         }
 
+
+        // GET: /Contact/
+        public ActionResult Index()
+        {
+            //return contactRepository.GetAllContacts();
+            Contact contact = new Contact();
+            ViewData["Contact"] = contact;
+            //return Controller
+            return View(contactRepository.GetAllContacts());
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+
+        // POST: /Contact/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ContactId,Name")] Contact contact)
+        {
+            if (ModelState.IsValid)
+            {
+                this.contactRepository.SaveContact(contact);
+                ViewData["Contact"] = contact;
+                ViewData["Contacts"] = contactRepository.GetAllContacts();
+                //var response = Request.CreateResponse<Contact>(System.Net.HttpStatusCode.Created, contact);
+                return RedirectToAction("Index");
+            }
+
+            return View(contact);
+        }
+
+        /*
         public Contact[] Get()
         {
             return contactRepository.GetAllContacts();
         }
-
+        
         public HttpResponseMessage Post(Contact contact)
         {
             this.contactRepository.SaveContact(contact);
@@ -32,7 +72,7 @@ namespace MvcRestfulContactManager.Controllers
 
             return response;
         }
-
+        */
 
     }
 }
